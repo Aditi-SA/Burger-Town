d44badf19e4df1a26f339e8811dc2432edc526a1
FionaViola <aditi.nadig10@gmail.com>
2018-12-13 17:43:41 -0500

    Updated format

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,22 +3,24 @@ var router = express.Router();
 
 var burger = require("../models/burger");
 
-router.get("/", function(req, res) {
-  burger.all(function(data) {
+router.get("/", function (req, res) {
+  burger.all(function (data) {
     console.log(data);
-    res.render("index", { burgers: data });
+    res.render("index", {
+      burgers: data
+    });
   });
 });
 
-router.post("/api/burger/", function(req, res) {
+router.post("/api/burger/", function (req, res) {
   console.log("Creating burger with name: " + req.body.burgerName);
-  burger.make(req.body.burgerName, function(result) {
+  burger.make(req.body.burgerName, function (result) {
     res.json(result);
   });
 });
 
-router.put("/api/burger/", function(req, res) {
-  burger.devour(req.body.id, function(result) {
+router.put("/api/burger/", function (req, res) {
+  burger.devour(req.body.id, function (result) {
     if (result.changedRows == 0) {
       res.status(404).end();
     } else {
